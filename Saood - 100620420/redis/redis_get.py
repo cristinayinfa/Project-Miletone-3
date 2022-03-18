0a87f6d82fd44a94efa6b148251e668288360d63
SaoodImran123 <90656881+SaoodImran123@users.noreply.github.com>
2022-03-18 00:08:45 -0400

    Delete Saood - 100620420 directory

--- Saood - 100620420/redis/redis_get.py
+++
@@ -1,7 +0,0 @@
-import redis        # pip install redis
-ip='34.130.141.192';
-r = redis.Redis(host=ip, port=6379, db=0,password='SOFE4630U')
-v=r.get('RandImages');
-
-with open("./recieved.jpg", "wb") as f:
-    f.write(v);
